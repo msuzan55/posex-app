2e77c43529a377bd54cc267d2796bc21b803f034
msuzan55 <msuzan55@users.noreply.github.com>
2026-07-26 19:04:41 +0530

    fix(ci): remove unused painting import in push_registration_service

--- lib/push/push_registration_service.dart
+++ lib/push/push_registration_service.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/painting.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:http/http.dart' as http;
